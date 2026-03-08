5f2676e2a2b5f75c9ff3a6fc4176be226c58b73c
Freezyre <noprobkkem@gmail.com>
2026-03-08 10:01:52 +0200

    fixed

--- main.py
+++ main.py
@@ -9,5 +9,5 @@ soup = BeautifulSoup(response.text, 'html.parser')
 books = soup.find_all('h3')
 
 for book in books:
-    title = book.find('a')['title']
-    print(title)    
+    titlee = book.find('a')['title']
+    print(titlee)    
